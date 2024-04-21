cb10b15753bea5411b493e4181051288dbcb77d2
Alexis-Prz21 <mp21030@ues.edu.sv>
2024-04-20 18:00:04 -0600

    Carga inicial del proyecto de Recordatorio de Medicación MediReminder.

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ProyectoPDM2024-GT2-Grup01-Tema"
+include(":app")
